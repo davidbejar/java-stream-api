3301ddc472d723a742cf0e7741afd1ef693616ed
David Bejar <david.bejar@gmail.com>
2016-11-21 00:42:22 +0700

    Replace stream() by parallelStream()

--- src/me/david/test/StreamJava8.java
+++ src/me/david/test/StreamJava8.java
@@ -15,8 +15,9 @@ public class StreamJava8 {
 	 */
 	public List<Integer> getOrderedGroceriesTransactionIds(List<Transaction> transactions) {
 		Comparator<Transaction> byValue = Comparator.comparing(Transaction::getValue);
-		List<Integer> transactionsIds = transactions.stream().filter(t -> t.getType() == TransactionType.GROCERIES)
-				.sorted(byValue.reversed()).map(Transaction::getId).collect(Collectors.toList());
+		List<Integer> transactionsIds = transactions.parallelStream()
+				.filter(t -> t.getType() == TransactionType.GROCERIES).sorted(byValue.reversed())
+				.map(Transaction::getId).collect(Collectors.toList());
 		return transactionsIds;
 	}
 
